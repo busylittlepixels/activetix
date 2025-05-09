42e956809bee490f97d899df7ebd6dcc1cc87923
Ronan O'Leary <roleary81@gmail.com>
2025-05-10 00:40:40 +0200

    attempt form config

--- components/feedback-form.jsx
+++ components/feedback-form.jsx
@@ -35,7 +35,7 @@ export function FeedbackForm() {
     return (
         <div className="w-full md:max-w-md">
             <Card title="Leave Feedback">
-                <form name="feedback" onSubmit={handleFormSubmit} className="flex flex-col gap-3 align-center">
+                <form name="feedback" onSubmit={handleFormSubmit} className="flex flex-col gap-3 align-center" data-netlify="true">
                     <input type="hidden" name="form-name" value="feedback" />
                     <input name="name" type="text" placeholder="Name" required className="input" />
                     <input name="email" type="email" placeholder="Email (optional)" className="input" />
