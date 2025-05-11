d0614ee8662c536d3fa5d445ecd522aa036664d0
Ronan O'Leary <roleary81@gmail.com>
2025-05-11 14:31:16 +0200

    pdate page

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -81,7 +81,7 @@ export default function ProfilePage() {
                   <svg className="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                     <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M17 16l4-4m0 0l-4-4m4 4H7m6 4v1a3 3 0 01-3 3H6a3 3 0 01-3-3V7a3 3 0 013-3h4a3 3 0 013 3v1" />
                   </svg>
-                  <span className="ml-3">Logout</span>
+                  <Link href="/login"><span className="ml-3">Logout</span></Link>
                 </button>
               </div>
             </div>
